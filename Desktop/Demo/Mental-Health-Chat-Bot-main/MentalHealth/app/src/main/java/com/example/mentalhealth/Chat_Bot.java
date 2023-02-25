e8580fb79da297090bc0395e1d449b9a69ab1460
dhruv0703 <115712795+dhruv0703@users.noreply.github.com>
2023-02-25 12:05:44 +0530

    Files Updated

---
+++ Desktop/Demo/Mental-Health-Chat-Bot-main/MentalHealth/app/src/main/java/com/example/mentalhealth/Chat_Bot.java
@@ -0,0 +1,22 @@
+package com.example.mentalhealth;
+
+public class Chat_Bot {
+    private String message;
+    private String sender;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
---
+++ Desktop/Demo/Mental-Health-Chat-Bot-main/MentalHealth/app/src/main/java/com/example/mentalhealth/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mentalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_bot);
+
+    }
+}
